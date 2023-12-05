a5816e138354a3d335e3674e4e9a00b6883faadd
Haidar <afiq.haidar12@gmail.com>
2023-12-06 04:13:57 +0700

    feat: lake base model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen">
-      div
-
+    <main className="flex flex-col h-screen w-screen mx-auto items-center justify-center">
+      <h1 className="font-bold text-2xl my-3">
+        Welcome to our Computer Graphics Final Project
+      </h1>
+      <Link href="/playground" className="p-4 bg-blue-300 rounded-md">
+        Press Here to play
+      </Link>
     </main>
   );
 }
