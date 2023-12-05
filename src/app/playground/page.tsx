cc8546cc9c600285d57c58751fd1728c03480c58
Adrian Karuna Soetikno <royankaruna@gmail.com>
2023-12-05 21:45:19 +0700

    added sound and light controls using focus

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -3,106 +3,175 @@ import { Canvas, useThree } from "@react-three/fiber";
 import React, { useState, useEffect, useRef } from "react";
 import Rain from "../components/Rain";
 import Timer from "../components/Timer";
-import { OrbitControls, PerspectiveCamera, SpotLight, useHelper } from "@react-three/drei";
+
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  SpotLight,
+  useHelper,
+} from "@react-three/drei";
 import { Kelas } from "../components/Kelas";
-import { DirectionalLight, DirectionalLightHelper, PointLight } from "three";
+import {
+  Audio,
+  DirectionalLight,
+  DirectionalLightHelper,
+  PointLight,
+} from "three";
 
 type cameraAngle = {
-	x: number;
-	y: number;
-	z: number;
+  x: number;
+  y: number;
+  z: number;
 };
 
 const Page = () => {
-	const [rainIntensity, setRainIntensity] = useState(0.4);
-	const [rainKey, setRainKey] = useState(0);
-	const [cameraLookAt, setCameraLookAt] = useState<cameraAngle>({
-		x: 20,
-		y: 4,
-		z: -7,
-	});
+  const [rainSound, setRainSound] = useState("/lowRain.mp3");
+  const [Focus, setFocus] = useState(0.5);
+  const [rainKey, setRainKey] = useState(0);
+  const [cameraLookAt, setCameraLookAt] = useState<cameraAngle>({
+    x: 20,
+    y: 0,
+    z: -7,
+  });
 
-	const handleIntensityChange = (newIntensity: number) => {
-		setRainIntensity(newIntensity);
-		console.log(newIntensity);
-		// Increment the key to trigger a re-mount of the Rain component
-		setRainKey((prevKey) => prevKey + 1);
-	};
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const handleIntensityChange = (newIntensity: number) => {
+    setFocus(newIntensity);
+    if (newIntensity > 0.5) {
+      if (rainSound != "heavyRain.mp3") {
+        setRainSound("heavyRain.mp3");
+        audioRef.current!.src = rainSound;
+        audioRef.current!.play();
+      }
+    } else {
+      if (rainSound != "lowRain.mp3") {
+        setRainSound("lowRain.mp3");
+        audioRef.current!.src = rainSound;
+        audioRef.current!.play();
+      }
+    }
+    console.log(newIntensity);
+    console.log(rainSound);
+    setRainKey((prevKey) => prevKey + 1);
+  };
 
-	const handleArrowKeyPress = (event: KeyboardEvent) => {
-		// Update the camera lookAt based on arrow key presses
-		switch (event.key) {
-			case "w":
-				setCameraLookAt((prev) => ({ ...prev, y: prev.y + 1 }));
-				break;
-			case "s":
-				setCameraLookAt((prev) => ({ ...prev, y: prev.y - 1 }));
-				break;
-			case "d":
-				setCameraLookAt((prev) => ({ ...prev, x: prev.x + 1 }));
-				break;
-			case "a":
-				setCameraLookAt((prev) => ({ ...prev, x: prev.x - 1 }));
-				break;
-			default:
-				break;
-		}
-	};
+  const handleArrowKeyPress = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case "w":
+        setCameraLookAt((prev) => ({ ...prev, y: prev.y + 1 }));
+        break;
+      case "s":
+        setCameraLookAt((prev) => ({ ...prev, y: prev.y - 1 }));
+        break;
+      case "d":
+        setCameraLookAt((prev) => ({ ...prev, x: prev.x + 1 }));
+        break;
+      case "a":
+        setCameraLookAt((prev) => ({ ...prev, x: prev.x - 1 }));
+        break;
+      default:
+        break;
+    }
+  };
 
-	useEffect(() => {
-		window.addEventListener("keydown", handleArrowKeyPress);
+  useEffect(() => {
+    audioRef.current!.src = rainSound;
+    audioRef.current!.play();
+    window.addEventListener("keydown", handleArrowKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleArrowKeyPress);
+    };
+  }, [rainSound]);
 
-		return () => {
-			window.removeEventListener("keydown", handleArrowKeyPress);
-		};
-	}, []);
+  const CameraLookAt = ({ x, y, z }: cameraAngle) => {
+    const { camera } = useThree();
+    useEffect(() => {
+      camera.lookAt(x, y, z);
+    }, [camera, x, y, z]);
+    return null;
+  };
 
-	const CameraLookAt = ({ x, y, z }: cameraAngle) => {
-		const { camera } = useThree();
-		useEffect(() => {
-			camera.lookAt(x, y, z);
-		}, [camera, x, y, z]);
-		return null;
-	};
+  const directionalLightRef = useRef<DirectionalLight>(null!);
+  const pointLightRef = useRef<PointLight>(null!);
+  return (
+    // <main className="h-screen w-screen">
 
-	const directionalLightRef = useRef<DirectionalLight>(null!);
-	const pointLightRef = useRef<PointLight>(null!);
-	return (
-		// <main className="h-screen w-screen">
-
-		// </main>
-		<main className="h-screen w-screen relative">
-			<div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
-				<Timer />
-			</div>
-			<div className="w-full h-full z-0">
-				{/* <button onClick={() => handleIntensityChange(Math.random())}>Change Rain Intensity</button> */}
-				<Canvas>
-					<directionalLight ref={directionalLightRef} position={[0, 20, -30]} intensity={1} color={"white"} castShadow />
-					{/* <pointLight
-		      ref={pointLightRef}
-		      position={[0, 20, -30]}
-		      intensity={200}
-		      color={"white"}
-		      castShadow
-		    /> */}
-					<pointLight ref={pointLightRef} position={[0, 5, 0]} intensity={20} color={"white"} castShadow />
-					<ambientLight intensity={0.2} />
-					{/* <OrbitControls /> */}
-					<PerspectiveCamera
-						makeDefault // Make this camera the default camera
-						position={[-7, 3.5, 2]} // Set the initial position of the camera
-						fov={60} // Set the field of view
-						near={0.1} // Set near clipping plane
-						far={100} // Set far clipping plane
-					/>
-					<CameraLookAt {...cameraLookAt} />
-					<Kelas />
-					<Rain key={rainKey} position={[2, 0, -12]} intensity={rainIntensity} />
-				</Canvas>
-			</div>
-		</main>
-	);
+    // </main>
+    <main className="h-screen w-screen relative">
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <Timer />
+      </div>
+      <div className="w-full h-full z-0">
+        <label>
+          Focus:
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={Focus}
+            onChange={(e) => handleIntensityChange(parseFloat(e.target.value))}
+          />
+          {Focus}
+        </label>
+        <audio ref={audioRef} controls onLoad={audioRef.current?.play}>
+          <source src={rainSound} type="audio/mp3" />
+        </audio>
+        <Canvas>
+          <directionalLight
+            ref={directionalLightRef}
+            position={[0, 20, -30]}
+            intensity={Focus + 0.1}
+            color={"white"}
+            castShadow
+          />
+          <pointLight
+            ref={pointLightRef}
+            position={[9.7, 5, -14]}
+            intensity={150}
+            distance={100}
+            decay={2}
+            color={"red"}
+          />
+          <pointLight
+            ref={pointLightRef}
+            position={[9.7, 5, -14]}
+            intensity={30}
+            distance={100}
+            decay={2}
+            color={"white"}
+          />
+          <pointLight
+            ref={pointLightRef}
+            position={[9.7, 4, -14]}
+            intensity={100}
+            distance={100}
+            decay={2}
+            color={"blue"}
+          />
+          <pointLight
+            ref={pointLightRef}
+            position={[0, 4.5, 0]}
+            intensity={20 * Focus}
+            color={"white"}
+            castShadow
+          />
+          <ambientLight intensity={Focus + 0.15} />
+          <PerspectiveCamera
+            makeDefault
+            position={[-7, 4, 2]}
+            fov={65}
+            near={0.1}
+            far={100}
+          />
+          <CameraLookAt {...cameraLookAt} />
+          {/* <OrbitControls /> */}
+          <Kelas />
+          <Rain key={rainKey} position={[2, 0, -12]} intensity={1 - Focus} />
+        </Canvas>
+      </div>
+    </main>
+  );
 };
 
 export default Page;
