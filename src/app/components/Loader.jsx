52be4c93fee0533c748bdcc6e0893d918fa8f482
Haidar <afiq.haidar12@gmail.com>
2023-12-07 05:02:52 +0700

    fix: loadedr and playground adjustmnet

--- src/app/components/Loader.jsx
+++ src/app/components/Loader.jsx
@@ -2,15 +2,25 @@
 import React from 'react'
 import { Canvas, useThree } from "@react-three/fiber";
 import { Loading } from "../components/loading/Loading";
+import { ClockLoader } from "react-spinners";
 
 const Loader = ({visibility}) => {
   return (
-    <div hidden className={` fixed flex justify-center items-center top-1/2 left-1/2 transition-all ease-in  duration-500 rounded-lg bg-[#EEF5FF] w-screen h-screen transform -translate-x-1/2 -translate-y-1/2 z-40 ${visibility}`}>
-      <Canvas >
-        <ambientLight intensity={1} />
-        <Loading />
-      </Canvas>
-    </div>
+    // <div hidden className={` fixed flex justify-center items-center top-1/2 left-1/2 transition-all ease-in  duration-500 rounded-lg bg-[#EEF5FF] w-screen h-screen transform -translate-x-1/2 -translate-y-1/2 z-40 ${visibility}`}>
+    //   <Canvas >
+    //     <ambientLight intensity={1} />
+    //     <Loading />
+    //   </Canvas>
+    // </div>
+    <div className={`fixed flex justify-center items-center top-1/2 left-1/2 transition-all ease-in  duration-500 rounded-lg  bg-black w-screen h-screen transform -translate-x-1/2 -translate-y-1/2 z-40 ${visibility}`}>
+
+    <ClockLoader
+      color="#6680fa"
+      cssOverride={{}}
+      loading
+      size={187}
+    />
+        </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Loading } from "./components/loading/Loading";
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen w-screen mx-auto items-center justify-center">
-      <h1 className="font-bold text-2xl my-3">
-        Welcome to our Computer Graphics Final Project
-      </h1>
-      <Link href="/playground" className="p-4 bg-blue-300 rounded-md">
-        Press Here to play
-      </Link>
+    <main className="h-screen w-screen relative">
+      <div className="fixed flex justify-center items-center flex-col top-1/2 left-1/2  transform -translate-y-1/2  -translate-x-1/2   z-50">
+        <h1 className="font-bold text-2xl my-3">
+          Welcome to our Computer Graphics Final Project
+        </h1>
+        <Link href="/playground" className="p-4 bg-blue-300 rounded-md">
+          Press Here to play
+        </Link>
+      </div>
+
+      <div
+        hidden
+        className={`fixed flex justify-center items-center top-1/2 left-1/2 transition-all ease-in  duration-500 rounded-lg bg-[#EEF5FF] w-screen h-screen transform -translate-x-1/2 -translate-y-1/2 z-0 `}
+      >
+        <Canvas>
+          <ambientLight intensity={1} />
+          <Loading />
+        </Canvas>
+      </div>
     </main>
   );
 }
